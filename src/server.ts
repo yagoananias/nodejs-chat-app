9006256ebfc45914220322507f731b1171428f31
Yago Ananias <yago_souza92@yahoo.com.br>
2021-04-20 13:07:23 -0300

    POST created

---
+++ src/server.ts
@@ -0,0 +1,13 @@
+import express, { request, response } from "express";
+
+const app = express();
+
+app.get("/", (request, response) => {
+  return response.send("Olá Yago :D");
+});
+
+app.post("/", (request, response) => {
+  return response.json({ message: "User created"});
+});
+
+app.listen(3333, () => console.log("Server is running on 3333"));
